f66f5cbd4fc4153f87b49a3225c6422030725336
Ramiro <rami.guzmi@gmail.com>
2022-04-07 10:50:37 -0300

    Avances

---
+++ RaycastTest/Assets/Scenes/movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] KeyCode Up;
+    [SerializeField] KeyCode Down;
+    [SerializeField] KeyCode Right;
+    [SerializeField] KeyCode Left;
+
+    [SerializeField] float velocityX;
+    [SerializeField] float velocityY;
+
+    CharacterController characterController;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(Up))
+        {
+            characterController.SimpleMove(transform.forward * velocityY);
+        }
+        if (Input.GetKey(Down))
+        {
+            characterController.SimpleMove(-transform.forward * velocityY);
+        }
+        if (Input.GetKey(Left))
+        {
+            transform.eulerAngles += Vector3.down * velocityX * Time.deltaTime;
+        }
+        if (Input.GetKey(Right))
+        {
+            transform.eulerAngles += Vector3.up * velocityX * Time.deltaTime;
+        }
+
+    }
+}
---
+++ RaycastTest/Assets/Scenes/raycast.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    RaycastHit hit;
+    MeshRenderer MR;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.up, out hit, 100f))
+        {
+            //hit.transform.gameObject.SetActive(false);
+            MR = hit.transform.gameObject.GetComponent<MeshRenderer>();
+            MR.enabled = false;
+        }
+        else
+        {
+            MR.enabled = true;
+        }
+    }
+}
